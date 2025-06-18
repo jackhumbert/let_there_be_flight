26f3a39fc03beebcce412c7455e5c619c0b7500f
Jack Humbert <jack.humb@gmail.com>
2025-06-17 21:59:59 -0400

    possibly fully version indpendent

--- src/red4ext/Main.cpp
+++ src/red4ext/Main.cpp
@@ -183,7 +183,7 @@ RED4EXT_C_EXPORT void RED4EXT_CALL Query(RED4ext::PluginInfo *aInfo) {
   aInfo->name = L"Let There Be Flight";
   aInfo->author = L"Jack Humbert";
   aInfo->version = RED4EXT_SEMVER(MOD_VERSION_MAJOR, MOD_VERSION_MINOR, MOD_VERSION_PATCH);
-  aInfo->runtime = RED4EXT_RUNTIME_LATEST;
+  aInfo->runtime = RED4EXT_RUNTIME_INDEPENDENT;
   aInfo->sdk = RED4EXT_SDK_LATEST;
 }
 
